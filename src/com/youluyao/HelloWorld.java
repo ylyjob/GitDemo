b6e9534d7b9986d4a8f29235dccad1c852f3c0f1
youluyao-ND <1031865195@qq.com>
2017-07-21 16:33:33 +0800

    第四次提交

--- src/com/youluyao/HelloWorld.java
+++ src/com/youluyao/HelloWorld.java
@@ -4,10 +4,23 @@ package com.youluyao;
  * Created by Administrator on 2017/7/21.
  */
 public class HelloWorld {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
         System.out.println("第三次提交");
+        HelloWorld helloWorld=new HelloWorld();
+        helloWorld.setName("youluyao");
+        System.out.println(helloWorld.getName());
     }
 
 }
